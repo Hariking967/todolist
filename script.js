31c313360b39c078e88adb325df0c9da389317b1
Hariking967 <harigallery9@gmail.com>
2025-03-07 18:16:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let todoList = [];
-dispList();
 
 function addTodo() {
     const inputElementName = document.querySelector('.js-name-input');
-    const name = inputElementName.value.trim();
     const inputElementDuedate = document.querySelector('.js-due-date-input');
+
+    const name = inputElementName.value.trim();
     const duedate = inputElementDuedate.value;
 
-    if (name === '') {
+    if (!name) {
         alert("Please enter a task name.");
         return;
     }
@@ -17,18 +17,19 @@ function addTodo() {
     inputElementName.value = '';
     inputElementDuedate.value = '';
 
-    dispList();
+    dispList(); // Update the display after adding a new item
 }
 
 function dispList() {
     const todoContainer = document.querySelector('.js-todo-list');
-    todoContainer.innerHTML = '';
+    todoContainer.innerHTML = ''; // Clear previous content
 
     todoList.forEach((todo, index) => {
         const todoItem = document.createElement('div');
+        todoItem.classList.add("todo-item");
         todoItem.innerHTML = `
             <div>${todo.name}</div>
-            <div>${todo.duedate}</div>
+            <div>${todo.duedate || "No due date"}</div>
             <button class="delete-todo-button" onclick="deleteTodo(${index});">Delete</button>
         `;
         todoContainer.appendChild(todoItem);
@@ -37,5 +38,5 @@ function dispList() {
 
 function deleteTodo(index) {
     todoList.splice(index, 1);
-    dispList();
+    dispList(); // Update the list after deletion
 }
